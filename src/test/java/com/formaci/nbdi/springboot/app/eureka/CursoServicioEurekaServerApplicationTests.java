cd53d9a7d7e1c49e5b94d0e15aa75f9fb1c4ef4e
CristianGaona <crgaonas24@gmail.com>
2020-04-08 17:35:31 -0500

    Creando servidor de registro Eureka

---
+++ src/test/java/com/formaci/nbdi/springboot/app/eureka/CursoServicioEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.formaci.nbdi.springboot.app.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoServicioEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
